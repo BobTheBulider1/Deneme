40d866746362de6d76668524ba7505161f7066b7
BobTheBulider1 <iboj1333@gmail.com>
2026-05-24 14:18:15 +0300

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://xhgslxymqkjqmibzhpmd.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoZ3NseHltcWtqcW1pYnpocG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NjMwMDMsImV4cCI6MjA5NTAzOTAwM30.MxHe40SKKgF-NKpmknr1pldFZOIp9D90eQ9_1czrZfo';
-
-// Initialize the Supabase client
-window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
